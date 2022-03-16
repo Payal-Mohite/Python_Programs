486cf97224bfe60363672869079f9f412f6e4e06
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-16 10:27:40 +0530

    Add files via upload
    Conversion of bits to mb,tb and gb

---
+++ 3_bits_mb,tb,gb.py
@@ -0,0 +1,21 @@
+# program to convert bits to Megabytes, Gigabytes and Terabytes
+b=float(input("Enter bits:"))     #bits
+
+
+mb=b*                             #megabytes
+print(b,"bits in megabytes:",mb)
+print(*********)
+
+gb=b*                           #gegabytes
+print(b,"bits in gigabytes:",gb)
+print(*********)
+
+tb=b*                            #terabytes
+print(b,"bits in terabytes:",tb)
+print(*********)
+
+
+
+
+
+
