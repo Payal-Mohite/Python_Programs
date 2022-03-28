9509c1b568b23b080cbcb419433cb840a66d666d
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-28 23:08:33 +0530

    Add files via upload

---
+++ 6_Area_perimeter_sq.py
@@ -0,0 +1,10 @@
+#Area and perimeter of square
+
+side=float(input("Enter sidelength of square: "))
+
+A=side*side
+P=4*side
+
+
+print("Area of a square is",A)
+print("perimeter of a square is:",P)
