78047fb0aa9c92ed466a1f7d5dd2fc2c8d1e62a4
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-30 22:29:22 +0530

    Add files via upload

---
+++ 7_Cylinder.py
@@ -0,0 +1,9 @@
+# calculate surface volume and area of a cylinder.
+radius=float(input("Enter radius of cylinder:"))
+height=float(input("Enter height of cylinder:"))
+
+Area=2*3.14*radius*height+2*3.14*radius*radius
+print("Area of cylinder:",Area)
+
+Volume=3.14*radius*radius*height
+print("Volume of cylinder:",Volume)
