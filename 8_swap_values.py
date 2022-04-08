9c4a98a56191c96158fa6f6763fafcc6c36b60fb
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-08 07:34:42 +0530

    Add files via upload

---
+++ 8_swap_values.py
@@ -0,0 +1,12 @@
+# swap the value of two variables
+
+a=20   #veriable1
+b=30   #variable2
+
+print("values before swapping:",a,b)
+t=0     #temporary variable
+t=a
+a=b
+b=t
+
+print("swapped values are:",a,b)
