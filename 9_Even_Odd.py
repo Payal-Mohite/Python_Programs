078eeefe6368c18b4543d380f56ea21670363d6e
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-08 18:36:39 +0530

    Add files via upload

---
+++ 9_Even_Odd.py
@@ -0,0 +1,12 @@
+#Even_Odd program
+
+no=int(input("Enter number to  check whether even or odd:"))
+
+if(no%2==0):
+    {
+        print("Givven number is Even")
+    }
+else:
+    {
+        print("Given number is odd")
+    }
