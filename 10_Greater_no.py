6a4012d2269677fb76a816ab4ec610531ea30adc
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-09 21:50:39 +0530

    Add files via upload

---
+++ 10_Greater_no.py
@@ -0,0 +1,12 @@
+#check the largest number among the three numbers
+a=10
+b=20
+c=30
+
+if(a>b and a>c):
+    print(a,"is greater number")
+elif(b>a and b>c):
+    print(b,"is greater number")
+elif(c>a and c>b):
+    print(c,"is greater number")
+    
