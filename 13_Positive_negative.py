33d7656ccf9c31d705b664fada3da3fe5362b1c6
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-12 21:17:12 +0530

    Add files via upload

---
+++ 13_Positive_negative.py
@@ -0,0 +1,9 @@
+#Number is Positive, Negative or Zero
+no=int(input("Enter number:"))
+
+if(no>0):
+    print(no,"is positive")
+elif(no<0):
+    print(no,"is negative")
+else:
+    print(no,"is zero")
