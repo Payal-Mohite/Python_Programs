d1ddd031a5a4bfad20b026dbd21d171e795842ca
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-21 19:43:10 +0530

    Add files via upload

---
+++ 17_1-100_Even.py
@@ -0,0 +1,34 @@
+#Python program to print all even numbers between 1 to 100 using while loop.
+'''
+#while
+i=2
+while(i<=100):
+    print(i)
+    i=i+2
+'''
+
+
+'''
+#While-using if statement
+i=2
+while(i<=100):
+    i+=1
+    if(i%2==0):
+        print(i)
+'''
+
+
+'''
+#for
+for i in range(2,100,2):
+    print(i)
+'''
+
+#for using if
+for i in range(101):
+    if(i%2==0):
+        print(i)
+
+
+    
+   
