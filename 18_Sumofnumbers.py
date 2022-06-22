e3869f0466ff82885681c0115158124d6e920e4d
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-06-22 18:23:40 +0530

    Create 18_Sumofnumbers.py

---
+++ 18_Sumofnumbers.py
@@ -0,0 +1,16 @@
+'''print("natural numbers till 10 are:")
+sum=0
+for i in range(1,11):
+    sum=sum+i
+    print(i)    
+print("Sum of these natural numbers is:",sum)'''
+
+sum=0
+i=1
+print("natural numbers till 10 are:")
+while(i<=10):     
+    print(i)
+    sum=sum+i
+    i+=1
+    
+print("Sum of these natural numbers is:",sum)
